5b68bf8aa01428703d470aceeca84907124f5086
mice2100 <mice2100@163.com>
2023-07-06 23:15:33 -0600

    tt

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 // Plugins
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -9,8 +11,35 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
+    }),
+    mkcert(),
+    VitePWA({
+      mode: 'development',
+      registerType: 'autoUpdate', 
+      devOptions: {
+        enabled: true
+      },
+      // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
@@ -37,6 +66,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    https: true,
     proxy: {
       '/npr': {
         target: 'http://localhost:8080',
